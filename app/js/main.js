bc2ffbec2f4da98731d1626552e14b75eb2c71fa
JungleHunter11 <nikitamagn@gmail.com>
2020-09-12 14:24:51 -0500

    added followers feed section with slider

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,8 @@ $(function () {
 		slidesToScroll: 3,
 		variableWidth: true,
 		speed: 1000,
+		prevArrow: '<button class="arrow followers-feed__arrow-left"><img class="template__img-arrow-left" src="../images/icons/chevron-left.svg" alt=""></button>',
+		nextArrow: '<button class="arrow followers-feed__arrow-right"><img class="template__img-arrow-right" src="../images/icons/chevron-right.svg" alt=""></button>'
 	});
 
 	var mixer = mixitup('.release-template');
